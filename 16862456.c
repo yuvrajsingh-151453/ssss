93d5eea817f20c1f31afa023736e7bcbaed92d9d
Anurag-Khichi <71366400+Anurag-Khichi@users.noreply.github.com>
2020-10-01 10:11:23 +0530

    Add files via upload

---
+++ 16862456.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+int main()
+{
+    int i,m,t,s,a,b,j,x,y,z;
+    scanf("%d",&t);
+    while(t--)
+    {
+        i=0;
+        y=0;
+        s=0;
+        scanf("%d%d",&a,&b);
+        if(a<b)
+        {
+            a=a+b;
+            b=a-b;
+            a=a-b;
+        }
+        m=a;
+        while(m>0)
+        {
+            m=m/10;
+            i++;
+        }
+        for(j=0;j<i;j++)
+        {
+            z=(a%10+b%10)%10;
+            s=s*10+z;
+            a=a/10;
+            b=b/10;
+        }
+        while(s>0)
+        {
+            y=(y*10)+(s%10);
+            s=s/10;
+        }
+        printf("%d\n",y);
+    }
+    return 0;
+}
