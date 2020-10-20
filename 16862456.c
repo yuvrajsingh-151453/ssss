2579cf6d4c7c35d586a7a2fe9781d6f504ffeef9
raj42614 <73151990+raj42614@users.noreply.github.com>
2020-10-20 09:04:40 +0530

    Update 16862456.c

--- 16862456.c
+++ 16862456.c
@@ -1,52 +1,111 @@
-// Optimized implementation of Bubble sort 
-#include <stdio.h> 
+// C++ program for Merge Sort 
+#include<iostream> 
+using namespace std; 
 
-void swap(int *xp, int *yp) 
+// Merges two subarrays of arr[]. 
+// First subarray is arr[l..m] 
+// Second subarray is arr[m+1..r] 
+void merge(int arr[], int l, int m, int r) 
 { 
-	int temp = *xp; 
-	*xp = *yp; 
-	*yp = temp; 
-} 
+	int n1 = m - l + 1; 
+	int n2 = r - m; 
 
-// An optimized version of Bubble Sort 
-void bubbleSort(int arr[], int n) 
-{ 
-int i, j; 
-bool swapped; 
-for (i = 0; i < n-1; i++) 
-{ 
-	swapped = false; 
-	for (j = 0; j < n-i-1; j++) 
+	// Create temp arrays 
+	int L[n1], R[n2]; 
+
+	// Copy data to temp arrays L[] and R[] 
+	for(int i = 0; i < n1; i++) 
+		L[i] = arr[l + i]; 
+	for(int j = 0; j < n2; j++) 
+		R[j] = arr[m + 1 + j]; 
+
+	// Merge the temp arrays back into arr[l..r] 
+	
+	// Initial index of first subarray 
+	int i = 0; 
+	
+	// Initial index of second subarray 
+	int j = 0; 
+	
+	// Initial index of merged subarray 
+	int k = l; 
+	
+	while (i < n1 && j < n2) 
 	{ 
-		if (arr[j] > arr[j+1]) 
+		if (L[i] <= R[j]) 
 		{ 
-		swap(&arr[j], &arr[j+1]); 
-		swapped = true; 
+			arr[k] = L[i]; 
+			i++; 
 		} 
+		else
+		{ 
+			arr[k] = R[j]; 
+			j++; 
+		} 
+		k++; 
 	} 
 
-	// IF no two elements were swapped by inner loop, then break 
-	if (swapped == false) 
-		break; 
-} 
+	// Copy the remaining elements of 
+	// L[], if there are any 
+	while (i < n1) 
+	{ 
+		arr[k] = L[i]; 
+		i++; 
+		k++; 
+	} 
+
+	// Copy the remaining elements of 
+	// R[], if there are any 
+	while (j < n2) 
+	{ 
+		arr[k] = R[j]; 
+		j++; 
+		k++; 
+	} 
 } 
 
-/* Function to print an array */
-void printArray(int arr[], int size) 
+// l is for left index and r is 
+// right index of the sub-array 
+// of arr to be sorted */ 
+void mergeSort(int arr[], int l, int r) 
 { 
-	int i; 
-	for (i=0; i < size; i++) 
-		printf("%d ", arr[i]); 
-	printf("n"); 
+	if (l < r) 
+	{ 
+		
+		// Same as (l+r)/2, but avoids 
+		// overflow for large l and h 
+		int m = l + (r - l) / 2; 
+
+		// Sort first and second halves 
+		mergeSort(arr, l, m); 
+		mergeSort(arr, m + 1, r); 
+
+		merge(arr, l, m, r); 
+	} 
 } 
 
-// Driver program to test above functions 
+// UTILITY FUNCTIONS 
+// Function to print an array 
+void printArray(int A[], int size) 
+{ 
+	for(int i = 0; i < size; i++) 
+		cout << A[i] << " "; 
+} 
+
+// Driver code 
 int main() 
 { 
-	int arr[] = {64, 34, 25, 12, 22, 11, 90}; 
-	int n = sizeof(arr)/sizeof(arr[0]); 
-	bubbleSort(arr, n); 
-	printf("Sorted array: \n"); 
-	printArray(arr, n); 
+	int arr[] = { 12, 11, 13, 5, 6, 7 }; 
+	int arr_size = sizeof(arr) / sizeof(arr[0]); 
+
+	cout << "Given array is \n"; 
+	printArray(arr, arr_size); 
+
+	mergeSort(arr, 0, arr_size - 1); 
+
+	cout << "\nSorted array is \n"; 
+	printArray(arr, arr_size); 
 	return 0; 
 } 
+
+// This code is contributed by Mayank Tyagi
